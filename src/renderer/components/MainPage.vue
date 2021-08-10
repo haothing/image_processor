7695f9b53c1b384f0101e61f678bbd2c5eff866b
haothing <crosstime.sun@gmail.com>
2021-08-10 16:38:24 +0900

    update

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="main-content">
-
     <div class="image-zone">
-        <img id="image-content" class="image-content" :src="currentImgPath" @mousewheel="onWheel" :style="imageTransforms">
+        <!-- <transition name="fade">
+            <img id="image-content" class="image-content" :class="{'image-content-fade': isImgFade}" v-show="isShowImg" :src="currentImgPath" :style="imageTransforms" 
+                @mousewheel="onWheel" @mousedown="onMouseDown" @mouseup="onMouseUp" @mouseout="onMouseOut" @mousemove="onMouseMove" @dblclick="onDbClick"
+                @load="onImgLoaded">
+        </transition> -->
+
+        <img id="image-content" class="image-content" :class="{'image-content-fade': isImgFade}" 
+            v-show="showImg" :src="currentImgPath" :style="imageTransforms" 
+            @mousewheel="onWheel" @mousedown="onMouseDown" @mouseup="onMouseUp" @mouseout="onMouseOut" @mousemove="onMouseMove" 
+            @dblclick="onDbClick" @load="onImgLoaded">
         <!-- <vue-cropper class="image-content" ref="cropper" :src="currentImgPath" :background="false" :autoCrop="false"/> -->
+    </div>
+    <div class="image-shadow" v-show="showImgShadow" @transitionend="transitionend">
+        <img id="image-shadow" class="image-content-shadow" :class="{'image-content-shadow-fade': isImgShadowFade}" :src="shadowImgPath">
     </div>
     <!-- <div class="gallery-zone" style="display:none">
         <ul id="images">
@@ -26,6 +37,11 @@
                     <font-awesome-icon :icon="['far', 'folder-open']"/>         
                 </span>
             </button>
+            <button class="button is-light is-rounded btn-fun" @click="testImage">
+                <span class="cus-icon">
+                    <font-awesome-icon :icon="['far', 'folder-open']"/>         
+                </span>
+            </button>
         </div>
     </div>
     <div class="nav-box nav-box-left" v-bind:class="{ navActive: isNavLeft }"
@@ -44,14 +60,16 @@ import { remote } from 'electron'
 // import 'cropperjs/dist/cropper.css'
 // import Viewer from 'viewerjs'
 // import 'viewerjs/dist/viewer.css'
-
+// import 'viewerjs/dist/viewer.css'
+import '@/assets/css/MainPage.css'
 export default {
   name: 'main-page',
   components: {},
   data () {
     return {
-      currentImgPath: 'F:/repositories/workspaces/image_processor/static/images/6.jpg',
-      currentImgIndex: 0,
+      currentImgPath: 'F:/repositories/workspaces/image_processor/static/images/5.jpg',
+      shadowImgPath: '',
+      currentImgIndex: 4,
       imgExt: ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg', 'tif', 'tiff'],
       imgPathInDir: ['F:/repositories/workspaces/image_processor/static/images/1.jpg',
         'F:/repositories/workspaces/image_processor/static/images/2.jpg',
@@ -60,6 +78,12 @@ export default {
         'F:/repositories/workspaces/image_processor/static/images/5.jpg'],
       isNavLeft: false,
       isNavRight: false,
+      isMouseDown: false,
+      showImg: true,
+      isImgFade: false,
+      showImgShadow: false,
+      isImgShadowFade: false,
+      moveable: false,
       transform: {
         scaleX: 1,
         scaleY: 1,
@@ -75,61 +99,15 @@ export default {
   computed: {
     imageTransforms: function () {
       return {
-        transform: `scale(${this.transform.scaleX})`,
+        transform: `scale(${this.transform.scaleX}, ${this.transform.scaleY}) translate(${this.transform.translateX}px, ${this.transform.translateY}px)`,
         transformOrigin: `${this.transform.originX}px ${this.transform.originY}px`
-        //  translate(${this.transform.translateX}px)
       }
     }
   },
-  created () {
-    // this.setCurrentFile('F:/repositories/workspaces/image_processor/static/images/1.jpg')
-    // this.$nextTick(() => {
-    //   console.log(document.getElementById('images').querySelectorAll('img'))
-    //   this.gallery = new Viewer(document.getElementById('images'))
-    // })
-  },
   mounted () {
-    // this.viewer = new Viewer(document.getElementById('image-content'), {
-    //   title: false,
-    //   inline: true,
-    //   movable: false,
-    //   button: false,
-    //   navbar: false,
-    //   fullscreen: true,
-    //   transition: false,
-    //   loading: false,
-    //   toolbar: false,
-    //   //   toolbar: {
-    //   //     zoomIn: 4,
-    //   //     zoomOut: 4,
-    //   //     oneToOne: {show: 1, size: 'large'},
-    //   //     reset: 4,
-    //   //     prev: {show: 1, size: 'small'},
-    //   //     play: {show: 1, size: 'large'},
-    //   //     next: 1,
-    //   //     rotateLeft: 4,
-    //   //     rotateRight: 4,
-    //   //     flipHorizontal: 4,
-    //   //     flipVertical: 4
-    //   //   },
-    //   view () {
-    //     console.log('1:' + new Date())
-    //     this.showViewer = false
-    //   },
-    //   viewed () {
-    //     console.log('2:' + new Date())
-    //     this.showViewer = true
-    //   },
-    //   zoomed () {
-    //     if (!this.viewer.options.movable) {
-    //       this.viewer.options.movable = true
-    //     }
-    //   }
-    // })
-    // this.gallery = new Viewer(document.getElementById('images'))
     document.addEventListener('drop', (e) => {
       e.preventDefault()
-      e.stopPropagation()
+      // e.stopPropagation()
       for (const f of e.dataTransfer.files) {
         this.setCurrentFile(f.path)
       }
@@ -150,15 +128,31 @@ export default {
         filters: [{ name: 'Images', extensions: this.imgExt }],
         properties: ['openFile']
       })
-      this.setCurrentFile(openImgPath[0])
+      if (openImgPath != null && openImgPath.length > 0) {
+        this.setCurrentFile(openImgPath[0])
+      }
     },
     resetImage () {
-      this.viewer.reset()
+      // this.viewer.reset()
+      this.moveable = false
+      this.transform = {
+        scaleX: 1,
+        scaleY: 1,
+        translateX: 0,
+        translateY: 0,
+        originX: 0,
+        originY: 0,
+        padX: 0,
+        padY: 0,
+        mouseX: 0,
+        mouseY: 0
+      }
     },
     setCurrentFile (filePath) {
       if (this.imgExt.indexOf(path.extname(filePath).toLowerCase().substr(1)) !== -1) {
         this.currentImgPath = filePath
         this.imgPathInDir = []
+        this.resetImage()
         // this.$nextTick(() => {
         //   this.viewer.update()
         // })
@@ -200,16 +194,33 @@ export default {
       }
     },
     nextImage () {
+      console.log('0 nextImage start')
       if (this.imgPathInDir.length > 0) {
-        this.currentImgIndex += 1
-        if (this.currentImgIndex >= this.imgPathInDir.length) {
-          this.currentImgIndex = 0
+        // this.isImgFade = false
+        console.log('1')
+        // if (!this.showImgShadow) {
+        if (this.showImg) {
+          console.log('2 set shadow path')
+          this.showImgShadow = true
+          this.isImgShadowFade = false
+          this.shadowImgPath = this.currentImgPath
+
+          this.currentImgIndex += 1
+          if (this.currentImgIndex >= this.imgPathInDir.length) {
+            this.currentImgIndex = 0
+          }
+          this.showImg = false
+          this.isImgFade = false
+          this.currentImgPath = this.imgPathInDir[this.currentImgIndex]
+          console.log('3 nextImage end')
+          // this.currentImgPath = this.imgPathInDir[this.currentImgIndex]
         }
-        this.currentImgPath = this.imgPathInDir[this.currentImgIndex]
-        console.log(this.currentImgPath)
-        // this.$nextTick(() => {
-        //  this.viewer.update()
-        // })
+
+        // setTimeout(() => { this.isImgShadowFade = true }, 50)
+        // this.showImg = false
+        // this.isImgFade = false
+        // setTimeout(() => {this.showImg = true}, 10)
+        // setTimeout(() => {this.isImgFade = true}, 20)
         // this.$refs.cropper.replace(this.imgPathInDir[this.currentImgIndex])
       }
     },
@@ -219,107 +230,117 @@ export default {
     displayNavRight () {
       this.isNavRight = !this.isNavRight
     },
+    testImage () {
+      this.showImg = false
+      this.isImgShadowFade = false
+      // set shadow element style
+      this.shadowImgPath = this.currentImgPath
+      this.showImgShadow = true
+      // this.isImgShadowFade = true
+      setTimeout(() => { this.isImgShadowFade = true }, 10)
+    },
+    onShadowImgLoaded: function () {
+      // this.isImgShadowFade = true
+      // setTimeout(() => { this.showImgShadow = false }, 1000)
+      console.log('4 shadow loaded')
+      if (!this.showImgShadow) {
+        console.log('5 shadow display')
+        this.showImgShadow = true
+        setTimeout(() => {
+          console.log('7 shadow set anime')
+          this.isImgShadowFade = true
+        }, 50)
+        console.log('6')
+      }
+    },
+    onImgLoaded: function () {
+      console.log('10 image load')
+      this.showImg = true
+      //   setTimeout(() => {   
+      //     this.showImg = true
+      //     // this.isImgFade = true
+      //     console.log('11 image displayed')
+      //   }, 10)
+      setTimeout(() => {
+        this.isImgFade = true
+        this.isImgShadowFade = true
+        console.log('12 image fade')
+      }, 20)
+      //   this.$nextTick(() => {
+      //     this.resetImage()
+      //   })
+    },
+    transitionend: function () {
+      console.log('20 transitionend')
+      this.showImgShadow = false
+      this.isImgShadowFade = false
+      console.log('21 end')
+    },
     onWheel: function (e) {
-    //   this.transform.translateX = e.x
-    //   this.transform.translateY = e.y
-      let w = e.srcElement.naturalWidth
-      let h = e.srcElement.naturalHeight
-      // let canvasW = e.srcElement.clientWidth
-      // let canvasH = e.srcElement.clientHeight
-
-      console.log(this.transform.padX, this.transform.padY)
-      this.transform.originX = e.x// + this.transform.padX
-      this.transform.originY = e.y// + this.transform.padY
-
-      let scale = 0.001 * e.deltaY
-      this.transform.scaleX -= scale
-      this.transform.scaleY -= scale
-
-      this.transform.padX = (w * scale)
-      this.transform.padY = (h * scale)
-
-      console.log(e.x + ';    ' + e.y + ';    ' + this.transform.scaleX + ';    ' + e.srcElement.naturalWidth)
       e.preventDefault()
       e.stopPropagation()
+
+      // Calculate the offset after resetting the origin
+      this.transform.padX = (this.transform.scaleX - 1) / this.transform.scaleX * (e.x - this.transform.originX)
+      this.transform.padY = (this.transform.scaleY - 1) / this.transform.scaleY * (e.y - this.transform.originY)
+
+      this.transform.translateX += this.transform.padX
+      this.transform.translateY += this.transform.padY
+
+      this.transform.originX = e.x
+      this.transform.originY = e.y
+
+      let deltaY = Math.abs(e.deltaY) > 200 ? e.deltaY / Math.abs(e.deltaY) * 200 : e.deltaY
+      let scale = (0.0007 * deltaY).toFixed(4)
+      console.log('scale', deltaY, scale)
+      let scaleX = this.transform.scaleX - scale
+      let scaleY = this.transform.scaleY - scale
+
+      // limit scale size from 15 to 0.1
+      if (scaleX < 15 && scaleX >= 0.1 && scaleY < 15 && scaleY >= 0.1) {
+        this.transform.scaleX = scaleX
+        this.transform.scaleY = scaleY
+      }
+      if (!this.moveable && scaleX > 1) {
+        this.moveable = true
+      }
+    },
+    onMouseDown: function (e) {
+      e.preventDefault()
+      e.stopPropagation()
+      this.isMouseDown = true
+      this.transform.mouseX = e.x
+      this.transform.mouseY = e.y
+    },
+    onMouseUp: function (e) {
+      e.preventDefault()
+      e.stopPropagation()
+      this.isMouseDown = false
+    },
+    onMouseOut: function (e) {
+      e.preventDefault()
+      e.stopPropagation()
+      this.isMouseDown = false
+    },
+    onMouseMove: function (e) {
+      e.preventDefault()
+      e.stopPropagation()
+      if (this.isMouseDown && this.moveable) {
+        // console.log((this.transform.translateX), (this.transform.translateY))
+        this.transform.translateX -= (this.transform.mouseX - e.x) / this.transform.scaleX
+        this.transform.translateY -= (this.transform.mouseY - e.y) / this.transform.scaleY
+
+        this.transform.mouseX = e.x
+        this.transform.mouseY = e.y
+      }
+    },
+    onDbClick: function (e) {
+      e.preventDefault()
+      e.stopPropagation()
+      this.resetImage()
     }
   }
 }
 </script>
 <style>
-html, body {
-    height: 100%;
-    width: 100%;
-    background-color: #000000;
-}
-.cus-icon {
-    color: #000000;
-}
-::-webkit-scrollbar {
-    display: none;
-}
-.main-content {
-    height: 100%;
-    width: 100%;
-}
-.image-zone {
-    height: 100%;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    overflow: hidden;
-}
-.action-bar-container {
-    position: fixed;
-    left: 8px;
-    right: 8px;
-    bottom: 10px;
-    text-align: center;
-}
-.action-bar {
-    width: 950px;
-    height: 60px;
-    margin: 0 auto;
-    display: flex;
-    justify-content: center;
-    background: rgba(255,255,255,.2); 
-}
-.btn-nav {
-    width: 160px;
-    margin: auto 10px;
-}
-.btn-fun {
-    width: 40px;
-    margin: auto 10px;
-}
-.action-box {
-    border-radius: 20px;
-    box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1), 0 0px 0 1px rgba(10, 10, 10, 0.02);
-}
-.nav-box-right {
-    position: fixed;
-    top: 0px;
-    right: 0px;
-    bottom: 0px;
-    width: 15%;
-    height: 100%;
-    background: rgba(255,255,255,.0); 
-}
-.nav-box-left {
-    position: fixed;
-    top: 0px;
-    left: 0px;
-    bottom: 0px;
-    width: 15%;
-    height: 100%;
-    background: rgba(255,255,255,.0); 
-}
-.navActive {
-    background: rgba(255,255,255,.1);
-}
-.image-content {
-    max-height:100%;
-    max-width:100%;
-    flex: 1 0 0;
-    object-fit: scale-down;
-}
 </style>
